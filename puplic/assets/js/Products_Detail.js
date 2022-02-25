0b08ce9f77bc1349d1f0f05868fae65f2f09e435
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2022-02-25 17:08:35 +0700

    Hoat-Update

--- puplic/assets/js/Products_Detail.js
+++ puplic/assets/js/Products_Detail.js
@@ -1,4 +1,4 @@
-/*slide*/
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -29,4 +29,59 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
-/*end slide*/
+//time down
+var t=document.getElementById("time")
+var d=2,h=23,m=55,s=22;
+var timeout=setInterval(counDown,1000)
+function counDown(){
+    t.innerHTML=(d+'D:'+h+'H:'+m+'M:'+s+'S')
+    s--;
+    if(s<0){
+        m--;
+        s=60
+    }else if (m<0){
+        h--;
+        m=60
+    }
+    else if(h<0){
+        d--;
+        h=24
+    }
+    else if(d<0||d==null||h==null||m==null||s==null) {
+        clearImmediate(timeout)
+        t.innerHTML="Time Ended";
+    }
+}
+//end timedown
+//start number change
+var active=document.getElementById('counter1');//number1
+var actives=setInterval(activeD,30)
+var n=0;
+function activeD(){
+    active.innerHTML=n;
+    n++;
+    if (n==69){
+        clearInterval(actives)
+    }
+}
+var active2=document.getElementById('counter2');//number2
+var active2s=setInterval(activeD2,10)
+var k=0;
+function activeD2(){
+    active2.innerHTML=k;
+    k++;
+    if (k==223){
+        clearInterval(active2s)
+    }
+}
+var active3=document.getElementById('counter3');//number2
+var active3s=setInterval(activeD3,30)
+var h=0;
+function activeD3(){
+    active3.innerHTML=h;
+    h++;
+    if (h==90){
+        clearInterval(active3s)
+    }
+}
+
