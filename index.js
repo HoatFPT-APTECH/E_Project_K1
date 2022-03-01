dbff8c8fd4e798b1c35bfc64a1baa76a4150d211
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2022-03-01 10:48:55 +0700

    Hoat-Update

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.get('/productDetail',function (req,res){
     var products=[
         {ID:132325456,
             item:45551,
-            nameProduct:'Xe Hồng Kỳ H9',
-        img1:'https://cdn.24h.com.vn/upload/2-2020/images/2020-04-20/1587367928-566-xe-hong-ky-h9-cua-trung-quoc-ra-mat-canh-tranh-voi-rolls-royce-989--10--1587361690-width660height440.jpg',
-        img2:'https://cdn.24h.com.vn/upload/2-2020/images/2020-04-20/1587367928-11-xe-hong-ky-h9-cua-trung-quoc-ra-mat-canh-tranh-voi-rolls-royce-989--16--1587361690-width660height371.jpg',
-        img3:'https://cdn.24h.com.vn/upload/2-2020/images/2020-04-20/1587367928-762-xe-hong-ky-h9-cua-trung-quoc-ra-mat-canh-tranh-voi-rolls-royce-989--7--1587361690-width660height440.jpg',
-        img4:'https://cdn.24h.com.vn/upload/2-2020/images/2020-04-20/1587367928-942-xe-hong-ky-h9-cua-trung-quoc-ra-mat-canh-tranh-voi-rolls-royce-989--12--1587361690-width660height440.jpg',
-        img5:'https://cdn.24h.com.vn/upload/2-2020/images/2020-04-20/1587367929-852-xe-hong-ky-h9-cua-trung-quoc-ra-mat-canh-tranh-voi-rolls-royce-989--17--1587361690-width660height495.jpg',
-        img6:'https://cdn.24h.com.vn/upload/2-2020/images/2020-04-20/1587367929-887-xe-hong-ky-h9-cua-trung-quoc-ra-mat-canh-tranh-voi-rolls-royce-989--11--1587361690-width660height440.jpg',
-        priceCurrent:70000,
-        bidIncrement:1000,
-        description:'Xe Hồng Kỳ H9 : Siêu xe hiện đang cạnh tranh trực tiếp với RollRoice'
+            nameProduct:'Xe Lamborghini Raventado',
+        img1:'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595097.jpg',
+        img2:'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595098.jpg',
+        img3:'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595104.jpg',
+        img4:'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595107.jpg',
+        img5:'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595097.jpg',
+        img6:'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595098.jpg',
+        priceCurrent:2000000,
+        bidIncrement:10000,
+        description:'Siêu xe duy nhất trên thế giới không có chiếc thứ 2'
         }
     ]
     res.render('Products_Detail',{
