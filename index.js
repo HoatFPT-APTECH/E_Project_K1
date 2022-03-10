9ed7a0eeeece9cf3d51d3c3bb75c86a6f189a044
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2022-03-10 20:44:01 +0700

    Hoat-Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const  express=require('express');
 const app=express();
-const port=8080;
+const port= process.env.PORT || 8080;
 
 app.listen(port,function (err){
     console.log('Serve is running. To check please access browser on : localhost:8080')
