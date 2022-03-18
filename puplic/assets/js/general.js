00d2060268e8e6ff0e820c3187f6e801cdb84de4
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2022-03-18 11:07:49 +0700

    Hoat-Update

--- puplic/assets/js/general.js
+++ puplic/assets/js/general.js
@@ -33,7 +33,7 @@ function closeButton(){
 //add to cart
  var products=JSON.parse(window.localStorage.getItem('products'));
  //{id: 1, img: 'https://media.laodong.vn/Storage/NewsPortal/2019/6/10/738354/595097.jpg', name: 'Xe Lamborghini Raventado', price: 300000}
-for (var i=0;i<products.length;i++){
+for (var i=0;i<products.length;i++){  //hien thi localstorge
     var cart=document.querySelector('.cart-products');
        var creatdiv=document.createElement('div');
 
@@ -41,28 +41,35 @@ for (var i=0;i<products.length;i++){
        creatdiv.innerHTML=divContent;
        cart.append(creatdiv);
 }
-function removeItem(nameRemove){
+function removeItem(nameRemove){  // data tranfers and detele item from localstorge
       function check(c){
           return c.name==nameRemove
       };
        var pR=products.find(check);
+       console.log(pR)
        products=products.filter(function (i){
            return i!=pR
        })
        window.localStorage.setItem('products',JSON.stringify(products));
 }
-var itemRemove=document.querySelectorAll('.remove-cart');
-itemRemove.forEach(function (btn,index){
-    btn.addEventListener('click',function (evn){
-        var itemRemove= evn.target.parentElement;
-        var nameRemove=itemRemove.querySelector('.content>.title>a').innerText;
-        removeItem(nameRemove)
-        itemRemove.parentElement.style.display='none';
-        let amount=products.length;
-        document.querySelector('.amount').innerHTML=amount
-    })
-});
-var amount=products.length;
-document.querySelector('.amount').innerHTML=amount
+function removeItemClient(){  // delete item from display and send name of product to function removeItem
+    var itemRemove=document.querySelectorAll('.remove-cart');
+    itemRemove.forEach(function (btn,index){
+        btn.addEventListener('click',function (evn){
+            var itemRemove= evn.target.parentElement;
+            var nameRemove=itemRemove.querySelector('.content>.title>a').innerHTML;
+            removeItem(nameRemove);
+            itemRemove.parentElement.style.display='none';
+            amount();
+        })
+    });
+}
+function amount(){ //display number products bought
+    document.querySelector('.amount').innerHTML=JSON.parse(window.localStorage.getItem('products')).length;
+}
+removeItemClient();
+amount();
+
+
 
 
